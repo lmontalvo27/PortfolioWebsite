09f198a9cb68f59c0e3fa1c216499ff597a5d5a7
Luis Montalvo <lmontalvo1998@gmail.com>
2024-03-30 20:49:01 -0700

    Setting up links and Navigation buttons

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.quetSelector(".menu-links");
+    const icon = document.quetSelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
